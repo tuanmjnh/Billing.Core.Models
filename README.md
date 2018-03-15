14e8d0f710f03c348224c32e6bb048609e0aa78e
Tuấn Hin <tuanmjnh@outlook.com>
2018-03-15 20:45:36 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Billing.Core.Models
---
+++ TEST.cs
@@ -0,0 +1,17 @@
+namespace Billing.Core.Models {
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.ComponentModel.DataAnnotations;
+    using System;
+
+    [Table("TEST")]
+    public partial class TEST {
+        [Dapper.Contrib.Extensions.ExplicitKey]
+        public Guid ID { get; set; }
+
+        [StringLength(512)]
+        public string NAME { get; set; }
+        public int LEVELS { get; set; }
+        public decimal MONEY { get; set; }
+        public DateTime? TIME { get; set; }
+    }
+}

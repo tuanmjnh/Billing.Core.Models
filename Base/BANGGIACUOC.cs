d7ecad0bf3e76bed871ea1f96788d729418761a6
Tuấn Hin <minhtuan200990@gmail.com>
2018-05-13 14:31:09 +0700

    tmvnpt

--- Base/BANGGIACUOC.cs
+++ Base/BANGGIACUOC.cs
@@ -3,20 +3,20 @@
     using System.ComponentModel.DataAnnotations;
     using System;
 
-    [Table("BANGGIACUOC")]
+    [Table ("BANGGIACUOC")]
     public partial class BANGGIACUOC {
         [Dapper.Contrib.Extensions.ExplicitKey]
         public Guid ID { get; set; }
 
-        [StringLength(256)]
+        [StringLength (256)]
         public string TENGOI { get; set; }
 
-        [StringLength(256)]
+        [StringLength (256)]
         public string PROFILE { get; set; }
         public int TOCDO { get; set; }
         public decimal GIA { get; set; }
 
-        [StringLength(128)]
+        [StringLength (128)]
         public string KIEU { get; set; }
         public int KIEUID { get; set; }
         public int GOICUOCID { get; set; }
@@ -26,17 +26,17 @@
         public DateTime? NGAYBATDAU { get; set; }
         public DateTime? NGAYKETTHUC { get; set; }
 
-        [StringLength(256)]
+        [StringLength (256)]
         public string CREATEDBY { get; set; }
         public DateTime? CREATEDAT { get; set; }
 
-        [StringLength(256)]
+        [StringLength (256)]
         public string UPDATEDBY { get; set; }
         public DateTime? UPDATEDAT { get; set; }
         public int EXTRA_TYPE { get; set; }
         public int FLAG { get; set; }
 
-        [StringLength(1024)]
+        [StringLength (1024)]
         public string GHICHU { get; set; }
     }
 }
